89a3eb08d5fdb1024e49a416c5877dbbc131239e
Gabriell0610 <gabrielbarbosaa432@gmail.com>
2025-10-25 16:54:04 -0300

    chore: Resolve CI error

--- src/app/client/orders/[id]/page.tsx
+++ src/app/client/orders/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/libs/auth';
 import ClientOrderDetailsPage from './clientPage';
-import { PageProps } from '.next/types/app/layout';
+import { PageProps } from '@/utils/types/generics/layout.type';
 
 export default async function OrderDetailsPage({ params }: PageProps) {
   const session = await getServerSession(authOptions);
