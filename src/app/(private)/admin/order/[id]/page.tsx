e31ef360a454fd8a31d47a5c1d78146cacffc6bf
Gabriell0610 <gabrielbarbosaa432@gmail.com>
2026-03-28 17:22:37 -0300

    fix: adjusting lib version

--- src/app/(private)/admin/order/[id]/page.tsx
+++ src/app/(private)/admin/order/[id]/page.tsx
@@ -2,5 +2,6 @@ import { PageProps } from '@/utils/types/generics/layout.type';
 import AdminOrderDetailsPage from './clientPage';
 
 export default async function OrderDetailsPage({ params }: PageProps) {
-  return <AdminOrderDetailsPage id={params.id} />;
+  const { id } = await params;
+  return <AdminOrderDetailsPage id={id} />;
 }
